1c3f276d129f91c4b66386e133f3de8456318bf4
Hugo Victor <hugovic.it@gmail.com>
2023-10-18 04:32:06 -0300

    7. Ojbetos esperados

--- test/CursoOnline.DominioTest/Cursos/CursoTest.cs
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -1,69 +1,64 @@
-﻿using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace CursoOnline.DominioTest.Cursos
+namespace CursoOnline.DominioTest.Cursos;
+
+/*
+"Eu, enquanto administrador, quero criar e editar
+cursos para que sejam abertas mastriculas para o mesmo."
+
+Criterios de aceite:
+->Criar yum curso com nome, carga horária, publico alvo e valor do curso.
+->As opções para público alvo são: Estudante, Universitário, Empregado e Empregador.
+->Todos os campos do curso são obrigatórios.
+ */
+public class CursoTest
 {
-
-    /*
-    "Eu, enquanto administrador, quero criar e editar
-    cursos para que sejam abertas mastriculas para o mesmo."
-
-    Criterios de aceite:
-    ->Criar yum curso com nome, carga horária, publico alvo e valor do curso.
-    ->As opções para público alvo são: Estudante, Universitário, Empregado e Empregador.
-    ->Todos os campos do curso são obrigatórios.
-     */
-
-    public class CursoTest
+    [Fact]
+    public void DeveCriarCurso()
     {
-
-
-        [Fact]
-        public void DeveCriarCurso()
+        //Arrange
+        var cursoEsperado = new
         {
+            Nome = "Informatica básica",
+            CargaHoraria = (double)80,
+            PublicoAlvo = "Estudantes",
+            Valor = (double)950
+        };
 
-            //Arrange
-            string nome = "Informatica básica";
-            double cargaHoraria = 80;
-            string publicoAlvo = "Estudantes";
-            double valor = 950;
+        //Action
+        var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
 
-            //Action
-            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+        //Assert
+        cursoEsperado.ToExpectedObject().ShouldMatch(curso);
+    }
+}
 
-            //Assert
-            Assert.Equal(nome, curso.Nome);
-            Assert.Equal(cargaHoraria, curso.CargaHoraria);
-            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
-            Assert.Equal(valor, curso.Valor);
-        }
+
+public class Curso
+{
+    public string Nome { get; private set; }
+    public double CargaHoraria { get; private set; }
+    public string PublicoAlvo { get; private set; }
+    public double Valor { get; private set; }
+    public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+    {
+        Nome = nome;
+        CargaHoraria = cargaHoraria;
+        PublicoAlvo = publicoAlvo;
+        Valor = valor;  
     }
 
 
-    public class Curso
-    {
-        public string Nome { get; private set; }
-        public double CargaHoraria { get; private set; }
-        public string PublicoAlvo { get; private set; }
-        public double Valor { get; private set; }
-        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
-        {
-            Nome = nome;
-            CargaHoraria = cargaHoraria;
-            PublicoAlvo = publicoAlvo;
-            Valor = valor;  
-        }
 
-
-
-       
-    }
-
-
+   
 }
 
 
